1025542d5e9f1721f001de6a5bd19437d452756a
Khatib-imen <114863318+Khatib-imen@users.noreply.github.com>
2025-11-11 21:31:44 +0100

    Update TypingAnimation.js

--- src/components/TypingAnimation.js
+++ src/components/TypingAnimation.js
@@ -7,7 +7,7 @@ const TypingAnimation = () => {
         options={{
           speed: 200,
           loop: true,
-          strings: [ "Developer"],
+          strings: [ "Web Developer"],
           breakLines: false,
         }}
       />
